6de5e4204e8af173ce0dab074bb425b85cbaddc3
Ahryxx <phvanthanh06@gmail.com>
2025-07-21 15:17:29 +0700

    Implement redirect from home route to /login and reorder chat route in authRoute.js

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+// Redirect trang chủ về /login
+router.get("/", (req, res) => res.redirect("/login"));
+
 router.get("/login", authController.getLoginPage);
 router.get("/register", authController.getRegisterPage);
-router.get("/chat", authController.getChatPage);
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+
+router.get("/chat", authController.getChatPage);
 
 module.exports = router;
